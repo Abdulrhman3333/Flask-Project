23650869e84b509945b028854a213ee43a6ab8de
Abdulrhman <amahfozbagasher@stu.kau.edu.sa>
2024-07-18 13:13:46 +0300

    everything works fine include the update from the page

--- app.py
+++ app.py
@@ -28,16 +28,26 @@ def Index():
 def insert():
     if request.method == "POST":
         flash("Data Inserted Successfully")
-        division = request.form['division']
-        tech = request.form['tech']
-        dep = request.form['dep']
-        site = request.form['site']
-        month = request.form['month']
-        year = request.form['year']
+        # division = request.form['division']
+        # tech = request.form['tech']
+        # dep = request.form['dep']
+        # site = request.form['site']
+        # month = request.form['month']
+        # year = request.form['year']
+
+        id_data = request.get_json()['id']
+        # date = request.get_json()['date']
+        division = request.get_json()['division']
+        tech = request.get_json()['tech']
+        dep = request.get_json()['dep']
+        site = request.get_json()['site']
+        month = request.get_json()['month']
+        year = request.get_json()['year']
+        is_checked = request.get_json()['is_checked']
         cur = mysql.connection.cursor()
         # cur.execute("INSERT INTO rdc (date, division, tech,dep, site, month, year, is_checked) VALUES (NOW(), '%s', '%s', '%s', '%s', '%s', '%s','no')", (division, tech, dep, site, month, year))
         # cur.execute(''' INSERT INTO rdc VALUES(%s,%s) ''',(division,tech))
-        cur.execute(''' INSERT INTO rdc VALUES(Null,NOW(),%s,%s,%s,%s,%s,%s,"no") ''',(division,tech,dep,site,month,year))
+        cur.execute(''' INSERT INTO rdc VALUES(%s,NOW(),%s,%s,%s,%s,%s,%s,"no") ''',(id_data,division,tech,dep,site,month,year))
 
         mysql.connection.commit()
         return redirect(url_for('Index'))
@@ -55,7 +65,19 @@ def delete(id_data):
 @app.route('/update', methods= ['POST', 'GET'])
 def update():
     if request.method == 'POST':
-        id_data = request.form['id']
+        # print("Header info: ", request.headers['Content-Type'])
+        flash("Data Updated Successfully")
+        # id_data = request.get_json()['id']
+        # # date = request.get_json()['date']
+        # division = request.get_json()['division']
+        # tech = request.get_json()['tech']
+        # dep = request.get_json()['dep']
+        # site = request.get_json()['site']
+        # month = request.get_json()['month']
+        # year = request.get_json()['year']
+        # is_checked = request.get_json()['is_checked']
+        # ===============================================================
+        id_data  = request.form['id']
         date = request.form['date']
         division = request.form['division']
         tech = request.form['tech']
@@ -67,15 +89,17 @@ def update():
         
 
         cur = mysql.connection.cursor()
-        cur.execute("""UPDATE rdc SET month=(%s)""", [month])
+        # cur.execute("""UPDATE rdc SET month=(%s)""", [month])
         # cur.execute("""UPDATE rdc SET month={}""".format(month))
-        # cur.execute('''UPDATE rdc SET date=%s, division=%s, dep=%s, site=%s,month=%s, year=%s, is_checked=%s 
-        #             WHERE id=%s ''', (date,division,dep,site,month,year,is_checked,id_data))
-        print(month)
-        flash("Data Updated Successfully")
-        # return redirect(url_for('Index'))
-        return redirect(url_for('Index'))
+        try:
+            # cur.execute("UPDATE rdc SET dep=%s, site=%s WHERE id=%s", (dep,site,id_data))
+            cur.execute("""UPDATE rdc SET division=%s,tech=%s, dep=%s, site=%s,month=%s, year=%s, is_checked=%s 
+                    WHERE id=%s """, (division,tech,dep,site,month,year,is_checked,id_data))
+            mysql.connection.commit()
+        except:
+            print("eroooorrrrr rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
 
+        return redirect(url_for('Index'))
 
 
 
